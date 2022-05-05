94f3df19a57060d884c8ab35038e86b74ba4c5b0
Matheus <matheushenr941@gmail.com>
2022-05-05 14:16:48 -0300

    feat: added hero component

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,5 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from "./hero.module.scss";
 
-export default function AllThe() {
-  return <div className={styles.component}></div>;
+export default function Hero() {
+  return (
+    <div className={styles.component}>
+      <div className={styles.text}>
+        <h1>
+          <span>Create Stunning</span>Email <br />
+          Campaigns
+        </h1>
+      </div>
+
+      <div>
+        <img className={styles.heroImage} src="/images/hero-image.jpg" alt="" />
+      </div>
+    </div>
+  );
 }
