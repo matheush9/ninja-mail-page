11a404ae47422eb742f590fec50c8a7ade7112e8
Matheus <matheushenr941@gmail.com>
2022-05-05 15:34:11 -0300

    feat: added video component

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -10,9 +10,16 @@ export default function Hero() {
           Campaigns
         </h1>
       </div>
+      <img className={styles.heroImage} src="/images/hero-image.jpg" alt="" />
 
-      <div>
-        <img className={styles.heroImage} src="/images/hero-image.jpg" alt="" />
+      <p>
+        Create and launch email campaigns that captivate your customers in just
+        a few clicks.
+      </p>
+
+      <div className={styles.btns}>
+        <button>TRY NOW</button>
+        <button>GET A DEMO</button>
       </div>
     </div>
   );
--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -1,5 +1,15 @@
 import styles from "./video.module.scss";
 
 export default function Video() {
-  return <div className={styles.component}></div>;
+  return (
+    <div className={styles.component}>
+      <div className={styles.container}>
+        <img src="/images/video.jpg" alt="" />
+        <div className={styles.videoText}>
+          <h2>Reach More Customers</h2>
+          <button>LEARN HOW</button>
+        </div>
+      </div>
+    </div>
+  );
 }
