b5540fc6df05988dc8fa41ab327d71561250e4c2
Matheus <matheushenr941@gmail.com>
2022-05-05 14:13:14 -0300

    feat: added mobile navbar

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,5 +1,5 @@
 import styles from "./hero.module.scss";
 
-export default function Hero() {
+export default function AllThe() {
   return <div className={styles.component}></div>;
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from "./navbar.module.scss";
+import Image from "next/dist/client/image";
 
 export default function Navbar() {
-  return <div className={styles.component}></div>;
+  return (
+    <div className={styles.component}>
+      <img
+        className={styles.logoImage}
+        alt=""
+        src={"/images/ninja-mail-logo.png"}
+      />
+      <button>SIGN UP FREE</button>
+    </div>
+  );
 }
