fdbc9d488625d0c01a5504127cfa45b58975ea83
Matheus <matheushenr941@gmail.com>
2022-05-05 16:47:39 -0300

    feat: added learnhow component

--- src/components/LearnHow/index.jsx
+++ src/components/LearnHow/index.jsx
@@ -1,5 +1,19 @@
+import react, { useState } from "react";
 import styles from "./learnhow.module.scss";
 
 export default function LearnHow() {
-  return <div className={styles.component}></div>;
+  const [state, setState] = useState(true);
+
+  const showMessage = () => {
+    setState(false);
+  };
+
+  return (
+    <div className={styles.component}>
+      <h3>Learn how others are reaching their audience easier than before.</h3>
+      <input type="email" placeholder="Enter your email" />
+      <p className={state ? styles.none : ""}>Thanks! Email receveid</p>
+      <button onClick={showMessage}>JOIN OUR LIST</button>
+    </div>
+  );
 }
