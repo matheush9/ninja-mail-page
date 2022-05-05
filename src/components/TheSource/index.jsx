94404dcd573bda102719a3c580ea31a29844643c
Matheus <matheushenr941@gmail.com>
2022-05-05 16:25:47 -0300

    feat: added TheSource component

--- src/components/TheSource/index.jsx
+++ src/components/TheSource/index.jsx
@@ -1,5 +1,14 @@
 import styles from "./thesource.module.scss";
 
 export default function TheSource() {
-  return <div className={styles.component}></div>;
+  return (
+    <div className={styles.component}>
+      <h2>The source for proven, engaging email campaigns</h2>
+      <p>
+        Whether you&apos;re a startup, small business, e-commerce store, or want
+        to promote your app, NinjaMail has the feature set tailored for your
+        business.
+      </p>
+    </div>
+  );
 }
