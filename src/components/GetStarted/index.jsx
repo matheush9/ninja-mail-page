bf414aff9321fa6d748c3bc5142cfe2ca3b803ed
Matheus <matheushenr941@gmail.com>
2022-05-06 14:38:56 -0300

    added component getstarted

--- src/components/GetStarted/index.jsx
+++ src/components/GetStarted/index.jsx
@@ -1,5 +1,10 @@
 import styles from "./getstarted.module.scss";
 
 export default function GetStarted() {
-  return <div className={styles.component}></div>;
+  return (
+    <div className={styles.component}>
+      <h3>Get started today!</h3>
+      <button>PICK A PLAN</button>
+    </div>
+  );
 }
