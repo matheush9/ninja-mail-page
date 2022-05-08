f14dc80e87590c883345e2963067500323126eb1
Matheus <matheushenr941@gmail.com>
2022-05-08 12:51:58 -0300

    feat: added desktop styles to getstarted, footer and brands components

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,37 +3,47 @@ import styles from "./footer.module.scss";
 export default function Footer() {
   return (
     <div className={styles.component}>
-      <img
-        className={styles.logoImage}
-        src="/images/ninja-mail-logo.svg"
-        alt=""
-      />
-      <div>
-        <ul>
-          <li>Features</li>
-          <li>Pricing</li>
-          <li>Services</li>
-          <li>Partners</li>
-          <li>About Us</li>
-          <li>Tutorials</li>
-          <li>Resources</li>
-          <li>Help Center</li>
-          <li>Templates</li>
-          <li>Case Studies</li>
-          <li>Medium</li>
-          <li>Twitter</li>
-          <li>Facebook</li>
-          <li>Instagram</li>
-          <li>LinkedIn</li>
-          <li>Contact US</li>
-          <li>Slack</li>
-          <li>Jobs</li>
-        </ul>
-        <hr />
-
+      <div className={styles.container}>
+        <img
+          className={styles.logoImage}
+          src="/images/ninja-mail-logo.svg"
+          alt=""
+        />
+        <div className={styles.wrapper}>
+          <ul>
+            <div>
+              <li>Features</li>
+              <li>Pricing</li>
+              <li>Services</li>
+              <li>Partners</li>
+            </div>
+            <div>
+              <li>About Us</li>
+              <li>Tutorials</li>
+              <li>Resources</li>
+              <li>Help Center</li>
+              <li>Templates</li>
+              <li>Case Studies</li>
+            </div>
+            <div>
+              <li>Medium</li>
+              <li>Twitter</li>
+              <li>Facebook</li>
+              <li>Instagram</li>
+              <li>LinkedIn</li>
+            </div>
+            <div>
+              <li>Contact US</li>
+              <li>Slack</li>
+              <li>Jobs</li>
+            </div>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <div className={styles.copyrightContainer}>
         <div>
           <p className={styles.raleway700}>
-            {" "}
             NinjaMail is a sample project for Quest AI.
           </p>
           <p className={styles.raleway700}>
@@ -42,10 +52,8 @@ export default function Footer() {
         </div>
         <br />
         <br />
-        <div>
-          <p className={styles.raleway700}>Terms & Conditions</p>
-          <p className={styles.raleway700}>Privacy Policy</p>
-        </div>
+        <p className={styles.raleway700}>Terms & Conditions</p>
+        <p className={styles.raleway700}>Privacy Policy</p>
       </div>
     </div>
   );
